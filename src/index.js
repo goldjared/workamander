3f30485ae2f2fae1e3a6c94ede4929033965bce4
jared <jaredgold@proton.me>
2023-05-27 22:19:34 -0400

    add hours track to timer, reset min every hour

--- src/index.js
+++ src/index.js
@@ -2,16 +2,22 @@ import './style.css';
 
 function timer() {
   let start = Date.now();
-  let minute = 0;
+  let minutes = 0;
+  let hours = 0;
   setInterval(function() {
   const elapsedTime = Date.now() - start;
   let currentTime = (elapsedTime / 1000).toFixed(1);
   console.log(currentTime);
   if(currentTime >= 60) {
-    minute++;
+    minutes++;
     start = Date.now()
-    console.log(minute)
-
+    console.log('this is min', minute)
+  }
+  if(minutes === 60) {
+    minutes = 0;
+    hours++;
+    console.log('this is min after reset', minutes);
+    console.log('this is hour after +', hours);
   }
 
 }, 100);
