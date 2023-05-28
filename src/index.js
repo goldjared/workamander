7eee82a79449806ec7a1d484b25575da87df2017
jared <jaredgold@proton.me>
2023-05-27 21:06:06 -0400

    create timer, incr minute value

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 import './style.css';
 
-console.log("heyo");
-console.log("spyder");
+function timer() {
+  let start = Date.now();
+  let minute = 0;
+  setInterval(function() {
+  const elapsedTime = Date.now() - start;
+  let currentTime = (elapsedTime / 1000).toFixed(1);
+  if(currentTime === 60) {
+    minute++;
+    start = Date.now()
+    console.log(minute)
+
+  }
+  console.log(currentTime);
+}, 100);
+
+
+}
+  console.log(timer());
