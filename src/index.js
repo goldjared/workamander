d04f1ea1803e77af3cf7e987f69e5d4ecddcfd05
jared <jaredgold@proton.me>
2023-06-16 12:31:51 -0400

    add second to constructor, update constructor names accordingly

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
 import "./style.css";
 class Clock {
   constructor() {
+    this.second = 0;
     this.min = 0;
     this.hour = 0;
   }
   tester() {
-    console.log('********************', this.min, this.hour, 'this is tester, min/hour')
+    console.log('********************',this.second, this.min, this.hour, 'this is tester, min/hour')
   }
   start() {
     let start = Date.now();
 
     setInterval(() => {
       const elapsedTime = Date.now() - start;
-      let currentTime = (elapsedTime / 1000).toFixed(1);
-      console.log(currentTime);
-      if (currentTime >= 60) {
+      this.second = (elapsedTime / 1000).toFixed(1);
+      console.log(this.second);
+      if (this.second >= 60) {
         this.min++;
+        this.second = 0;
         start = Date.now();
         console.log("this is min", this.min);
       }
@@ -34,16 +36,24 @@ class Clock {
 }
 let clockX = new Clock();
 clockX.start();
-// setTimeout(() => {
-//   clockX.stop();
-// }, 5000);
+setTimeout(() => {
+  clockX.stop();
+}, 5000);
 setTimeout(() => {
   clockX.tester();
-}, 3000);
+}, 6000);
+setTimeout(() => {
+  clockX.start();
+}, 7000);
+setTimeout(() => {
+  clockX.tester();
+}, 8000);
+
+
 
 // setTimeout(() => {
 //   clockX.start();
 // }, 7000);
-setTimeout(() => {
- clockX.tester(); 
-}, 4000);
+// setTimeout(() => {
+//  clockX.tester(); 
+// }, 4000);
