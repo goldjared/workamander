17d7c338e469661cc9afe61a7845693b32be9fe5
jared <jaredgold@proton.me>
2023-06-16 12:25:57 -0400

    reformat timer into class, update functionality

--- src/index.js
+++ src/index.js
@@ -1,37 +1,38 @@
 import "./style.css";
 // also i was thinking, could my timer just do the min, and sec? and every interval it will also call the display
 // func, which will output HOUR:MINUTE:SEC? just some ideas
-function timer() {
-  const starter = () => {
-    let start = Date.now();
-    let minutes = 0;
-    let hours = 0;
+// function timer() {
+//   const starter = () => {
+//     let start = Date.now();
+//     let minutes = 0;
+//     let hours = 0;
 
-    setInterval(() => {
-      const elapsedTime = Date.now() - start;
-      let currentTime = (elapsedTime / 1000).toFixed(1);
-      console.log(currentTime);
-      if (currentTime >= 60) {
-        minutes++;
-        start = Date.now();
-        console.log("this is min", minutes);
-      }
-      if (minutes === 60) {
-        minutes = 0;
-        hours++;
-        console.log("this is min after reset", minutes);
-        console.log("this is hour after +", hours);
-      }
-    }, 100);
-  };
+//     setInterval(() => {
+//       const elapsedTime = Date.now() - start;
+//       let currentTime = (elapsedTime / 1000).toFixed(1);
+//       console.log(currentTime);
+//       if (currentTime >= 60) {
+//         minutes++;
+//         start = Date.now();
+//         console.log("this is min", minutes);
+//       }
+//       if (minutes === 60) {
+//         minutes = 0;
+//         hours++;
+//         console.log("this is min after reset", minutes);
+//         console.log("this is hour after +", hours);
+//       }
+//     }, 100);
+//   };
 
-  function stop() {
-    clearInterval(2);
-    console.log("fuck!");
-  }
+//   function stop() {
+//     clearInterval(2);
+//     console.log("fuck!");
+//   }
 
-  return { stop, starter };
-}
+//   return { stop, starter };
+// }
+
 /* lets say interval is 1, and alarm is cl ("fucl!")
  there is the main timer running. every 5 min
 it should fire something. with the minutes and hours.
@@ -42,8 +43,60 @@ this something will then work.
 
 // }
 
-timer().starter();
+// timer().starter();
+// setTimeout(() => {
+//   timer().stop();
+// }, 5000);
+// // timer().stop();
+// setTimeout(() => {
+//   timer().starter();
+// }, 6000);
+
+class Clock {
+  constructor() {
+    this.min = 0;
+    this.hour = 0;
+  }
+  tester() {
+    console.log('********************', this.min, this.hour, 'this is tester, min/hour')
+  }
+  start() {
+    let start = Date.now();
+
+    setInterval(() => {
+      const elapsedTime = Date.now() - start;
+      let currentTime = (elapsedTime / 1000).toFixed(1);
+      console.log(currentTime);
+      if (currentTime >= 60) {
+        this.min++;
+        start = Date.now();
+        console.log("this is min", this.min);
+      }
+      if (this.min === 60) {
+        this.min = 0;
+        this.hour++;
+        console.log("this is min after reset", this.min);
+        console.log("this is hour after +", this.hour);
+      }
+    }, 100);
+  }
+  stop() {
+        clearInterval(2);
+    console.log("fuck!");
+  }
+}
+let clockX = new Clock();
+clockX.start();
+// setTimeout(() => {
+//   clockX.stop();
+// }, 5000);
 setTimeout(() => {
-  timer().stop();
-}, 5000);
-// timer().stop();
+  clockX.tester();
+}, 3000);
+
+// setTimeout(() => {
+//   clockX.start();
+// }, 7000);
+setTimeout(() => {
+ clockX.tester(); 
+}, 4000);
