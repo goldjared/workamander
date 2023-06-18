c879b88aff8b62c2bda24d6160aa281bfa47edc2
jared <jaredgold@proton.me>
2023-06-17 23:08:09 -0400

    switch clock from constructor to factory func, tweak names in necessary funcs

--- src/index.js
+++ src/index.js
@@ -4,47 +4,89 @@ import { pausePlay } from "./operations";
 clockDisplay();
 clockUpdate();
 pausePlayBtn();
-class Clock {
-  constructor() {
-    this.timer;
-    this.second = 0;
-    this.min = 0;
-    this.hour = 0;
-    this.startDate = 0;
-    this.pauseTime = 0;
-    this.isPaused = true;
-  }
-  start() {
-    if (this.pauseTime != 0) {
-      this.startDate += Date.now() - this.pauseTime;
-      this.pauseTime = 0;
+function clock() {
+  let timer;
+  let seconds = 0;
+  let minutes = 0;
+  let hours = 0;
+  let startTime = 0;
+  let pauseTime = 0;
+  let isPaused = true;
+
+  function start() {
+    if (pauseTime != 0) {
+      startTime += Date.now() - pauseTime;
+      pauseTime = 0;
     } else {
-      this.startDate = Date.now();
+      startTime = Date.now();
     }
 
-    this.timer = setInterval(() => {
-      const elapsedTime = Date.now() - this.startDate;
-      this.second = (elapsedTime / 1000).toFixed(1);
-      console.log(this.second);
-      if (this.second >= 60) {
-        this.min++;
-        this.second = 0;
-        this.startDate = Date.now();
+    timer = setInterval(() => {
+      const elapsedTime = Date.now() - startTime;
+      seconds = (elapsedTime / 1000).toFixed(1);
+      console.log(seconds);
+      if (seconds >= 60) {
+        minutes++;
+        seconds = 0;
+        startTime = Date.now();
       }
-      if (this.min === 60) {
-        this.min = 0;
-        this.hour++;
+      if (minutes === 60) {
+        minutes = 0;
+        hours++;
       }
-      clockUpdate(this.hour, this.min, this.second);
+      clockUpdate(hours, minutes, seconds);
     }, 100);
   }
-  stop() {
-    clearInterval(this.timer);
-    this.pauseTime = Date.now();
-    clearInterval(this.timer);
+    function stop() {
+    pauseTime = Date.now();
+    clearInterval(timer);
 
     console.log("Stop function fired!");
-  }
+    }
+  return { isPaused, start, stop }
+  
 }
-let clockX = new Clock();
+// class Clock {
+//   constructor() {
+//     this.timer;
+//     this.second = 0;
+//     this.min = 0;
+//     this.hour = 0;
+//     this.startDate = 0;
+//     this.pauseTime = 0;
+//     this.isPaused = true;
+//   }
+//   start() {
+//     if (this.pauseTime != 0) {
+//       this.startDate += Date.now() - this.pauseTime;
+//       this.pauseTime = 0;
+//     } else {
+//       this.startDate = Date.now();
+//     }
+
+//     this.timer = setInterval(() => {
+//       const elapsedTime = Date.now() - this.startDate;
+//       this.second = (elapsedTime / 1000).toFixed(1);
+//       console.log(this.second);
+//       if (this.second >= 60) {
+//         this.min++;
+//         this.second = 0;
+//         this.startDate = Date.now();
+//       }
+//       if (this.min === 60) {
+//         this.min = 0;
+//         this.hour++;
+//       }
+//       clockUpdate(this.hour, this.min, this.second);
+//     }, 100);
+//   }
+//   stop() {
+//     this.pauseTime = Date.now();
+//     clearInterval(this.timer);
+
+//     console.log("Stop function fired!");
+//   }
+// }
+// let clockX = new Clock();
+let clockX = clock();
 pausePlay(clockX)
--- src/operations.js
+++ src/operations.js
@@ -35,5 +35,51 @@ function pausePlay(clockName) {
     }
   });
 };
+/*
+breaks
+  breaks will notify with sound
 
+call pausePlay to stop timer
+*/
+function timeEvent(name, breakTime, sound, timeStoppage) {
+  
+  return { name, breakTime, sound, timeStoppage}
+}
+// function pausePlay(clockName) {
+//   let cooldown = false;
+
+//   function pausePlayOps(e) {
+//     if(cooldown === true) {
+//       console.log('cooldown, 2 seconds');
+//       return;
+//     }
+
+//     cooldown = true;
+//     setTimeout(() => {
+//       cooldown = false;
+//     }, 300)
+    
+//     if (clockName.isPaused === false) {
+//       clockName.isPaused = true;
+//       clockName.stop();
+//       console.log("STOPPER");
+//     } else {
+//       clockName.isPaused = false;
+//       clockName.start();
+//       console.log(
+//         "STARTER, clockispaused:",
+//         clockName.isPaused,
+//         "should be false"
+//       );
+//     }
+//   }
+//   const pPBtn = document.querySelector(".pause-play");
+//   pPBtn.addEventListener("click", pausePlayOps);
+//   document.addEventListener("keydown", (e) => {
+//     if (e.code === "Space") {
+//       console.log(e, "fired, pausePops called");
+//       pausePlayOps();
+//     }
+//   });
+// };
 export { pausePlay }
