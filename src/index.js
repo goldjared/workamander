6f51a82fd8f3ca5abb8e7ec393ddbc1c4015d40e
jared <jaredgold@proton.me>
2023-06-17 23:41:52 -0400

    remove old code/debug, update import/export

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./style.css";
 import { clockDisplay, clockUpdate, pausePlayBtn } from "./display";
-import { pausePlay } from "./operations";
+import { pausePlay, timeEvent } from "./operations";
 clockDisplay();
 clockUpdate();
 pausePlayBtn();
@@ -46,47 +46,5 @@ function clock() {
   return { isPaused, start, stop }
   
 }
-// class Clock {
-//   constructor() {
-//     this.timer;
-//     this.second = 0;
-//     this.min = 0;
-//     this.hour = 0;
-//     this.startDate = 0;
-//     this.pauseTime = 0;
-//     this.isPaused = true;
-//   }
-//   start() {
-//     if (this.pauseTime != 0) {
-//       this.startDate += Date.now() - this.pauseTime;
-//       this.pauseTime = 0;
-//     } else {
-//       this.startDate = Date.now();
-//     }
-
-//     this.timer = setInterval(() => {
-//       const elapsedTime = Date.now() - this.startDate;
-//       this.second = (elapsedTime / 1000).toFixed(1);
-//       console.log(this.second);
-//       if (this.second >= 60) {
-//         this.min++;
-//         this.second = 0;
-//         this.startDate = Date.now();
-//       }
-//       if (this.min === 60) {
-//         this.min = 0;
-//         this.hour++;
-//       }
-//       clockUpdate(this.hour, this.min, this.second);
-//     }, 100);
-//   }
-//   stop() {
-//     this.pauseTime = Date.now();
-//     clearInterval(this.timer);
-
-//     console.log("Stop function fired!");
-//   }
-// }
-// let clockX = new Clock();
 let clockX = clock();
 pausePlay(clockX)
